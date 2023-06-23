cc91082e8f00fa7732de5e9925442a288b909029
sumanthkulkarni13 <sumanthkulkarni13@gmail.com>
2023-06-23 13:24:13 +0530

    Update docker-rpm-installer.sh

--- docker-rpm-installer.sh
+++ docker-rpm-installer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ## Install latest version of Docker Community version in a rpm based repo
 
+## Source - https://docs.docker.com/engine/install/centos/
+
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
