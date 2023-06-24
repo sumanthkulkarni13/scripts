2bf233eb3f111e72cd6bce0112d0decb584c9e52
sumanthkulkarni13 <sumanthkulkarni13@gmail.com>
2023-06-24 09:45:21 +0530

    Update rpm-bootstrapping.sh

--- rpm-bootstrapping.sh
+++ rpm-bootstrapping.sh
@@ -21,4 +21,21 @@ echo "    You should then see 'SELinux status: disabled' to confirm that SELinux
 echo ""
 
 ## 2. Install basic tools
-sudo yum install -y bind-tools net-tools telnet
+sudo yum install -y bind-tools net-tools telnet wget
+
+## 3. Install Python 3.9
+echo "Python 3.9 Installing... "
+echo "Installation will take 5-10mins as the binaries are compied from source"
+sleep 3
+sudo yum  -y groupinstall 'development tools'
+sudo yum -y install wget openssl-devel bzip2-devel libffi-devel
+sudo curl https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz -O
+tar -xvf Python-3.9.1.tgz
+cd Python-3.9.1
+sudo ./configure --enable-optimizations
+sudo make install
+python3 -V
+echo "Python 3.9 Install Complete"
+
+
+
